85e96aaf45b50cfd877720ef1a8289f6360f7032
Andres <andreangarita31@gmail.com>
2023-01-08 05:26:07 -0500

    se integraron la lista de carrito y el menu en version mobile y se soluciono prioridad para mostrar

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const menuEmail=document.querySelector(".navbar-email");
 const menuDesktop=document.querySelector(".desktop-menu");
+const menuMoIcon=document.querySelector(".menu");
+const mobileMenu=document.querySelector(".mobile-menu");
+const carIcon=document.querySelector(".navbar-shopping-cart");
+const lisCarCheck=document.querySelector(".product-detail");
 
-menuEmail.addEventListener("click",toggleDesktopMenu);
+menuEmail.addEventListener("click",toggleMD);
+menuMoIcon.addEventListener("click",toggleMM);
+carIcon.addEventListener("click",togglecar);
 
-function toggleDesktopMenu(){
-    menuDesktop.classList.toggle('inactive');
-} 
+function togglecar(){
+    const isMBclosed= mobileMenu.classList.contains ("inactive");
+    /* abrir car list */
+     if (!isMBclosed){
+        mobileMenu.classList.add("inactive");
+    };
+    lisCarCheck.classList.toggle("inactive");
+} ;
 
+function toggleMD(){
+    menuDesktop.classList.toggle('inactive');  
 
+}
+function toggleMM(){
+    const isLCclosed=lisCarCheck.classList.contains("inactive")
+    if (!isLCclosed){
+        lisCarCheck.classList.add("inactive");
+    }
+    mobileMenu.classList.toggle("inactive")
+}
 
 
 
